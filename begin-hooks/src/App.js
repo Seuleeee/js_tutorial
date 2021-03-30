d56e7dd2cc291986badfd0c918f51c885be2c178
조한슬 <hsjo@surromind.ai>
2021-03-30 19:36:25 +0900

    8장 hook 까지

---
+++ begin-hooks/src/App.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+import Counter from './Counter';
+import Info from './Info';
+import Average from './Average';
+
+
+const App = () => {
+  const [visible, setVisible] = useState(false);
+  return (
+    <div>
+      <div>
+        <button onClick={() => {setVisible(!visible);}}>
+          {visible ? '숨기기' : '보이기'}
+        </button>
+        <hr />
+        {visible && <Info/>}
+        {visible && <Counter/>}
+      </div>
+      <div>
+        <Average/>
+        <Info/>
+      </div>
+    </div>
+  )
+}
+
+export default App;
