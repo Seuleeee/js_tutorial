2a9f93535db64b7a6610553d749da1de068473ca
조한슬 <hsjo@surromind.ai>
2021-03-31 17:48:13 +0900

    9장 styled component 학습 완료

---
+++ chap9_component_styling/src/CSSModule.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from './CSSModule.module.scss';
+const CSSModule = () => {
+    return (
+        <div className={`${styles.wrapper} ${styles.inverted}`}>
+            Hi, I'm <span className="something">CSS Module!</span>
+        </div>
+    )
+}
+
+export default CSSModule;
